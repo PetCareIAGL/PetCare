cd4d2083003adc829edb462749673ffb36c18ed7
giss04 <gisselta04@hotmail.com>
2015-10-09 15:25:49 +0200

    traduction de la view modifier mot de passe

--- WebApplication1/Models/ManageViewModels.cs
+++ WebApplication1/Models/ManageViewModels.cs
@@ -47,18 +47,18 @@ namespace WebApplication1.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Mot de passe")]
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} doit être d'au moins {2} caractères.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nouveau mot de passe")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Confirmer nouveau mot de passe")]
+        [Compare("NewPassword", ErrorMessage = "La confirmation du nouveau mot de passe ne correspond pas.")]
         public string ConfirmPassword { get; set; }
     }
 
